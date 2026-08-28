e64f24feafbfce3c9c679266fc41624fd8d27f80
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-08-28 14:52:26 +0800

    feat: route fetch through dry-run wrapper

--- pygit/application.py
+++ pygit/application.py
@@ -16,7 +16,7 @@ from .checkout_index_cli import run_checkout_index
 from .clone_cli import run_clone
 from .commit_graph_cli import run_commit_graph
 from .count_objects_cli import run_count_objects
-from .fetch_cli import run_fetch
+from .fetch_cli_dry_run import run_fetch
 from .for_each_ref_cli import run_for_each_ref
 from .fsck_cli import run_fsck
 from .gc_cli import run_gc
